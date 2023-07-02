fd3c8d122b3071d0db1f7589fa6de8742c9f23f0
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 20:53:00 +0300

    Update 4.07.c (retab)

--- 4 "The C Programming Language"/4.07.c
+++ 4 "The C Programming Language"/4.07.c
@@ -2,17 +2,17 @@
 
 int spaces_count(const char *s)
 {
-	int spc = 0;
-	for( ; *s; s++) {
-		if(*s == ' ')
-			spc++;
-	}
-	return spc;
+    int spc = 0;
+    for( ; *s; s++) {
+        if(*s == ' ')
+            spc++;
+    }
+    return spc;
 }
 
 int main(int argc, char **argv)
 {
-	if(argc > 1)
-		printf("%d\n", spaces_count(argv[1]));
-	return 0;
+    if(argc > 1)
+        printf("%d\n", spaces_count(argv[1]));
+    return 0;
 }
