192e2a6857e740fc6762c68bbca306a500148eb3
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 19:11:35 +0300

    4.09.1b.c +

---
+++ 4 "The C Programming Language"/4.09.1b.c
@@ -0,0 +1,25 @@
+/* Number of words with an odd/even number of letters */
+#include <stdio.h>
+
+int main()
+{
+	int c, wrd, letters, odd, even;
+	wrd = letters = odd = even = 0;
+	while((c = getchar()) != EOF) {
+		if(c != '\n' && c != ' ' && c != '\t') { /* non-whitespace char */
+			if(!wrd)
+				wrd = 1;						 /* word indicator */
+			letters++;
+			continue;							 /* next char */
+		}
+		if(wrd)									 /* the word is over */	
+			letters % 2 == 0 ? even++ : odd++;	 /* counting letters */
+		if(c == '\n') {							 /* end of the line */
+			printf("odd: %d, even: %d\n", odd, even); /*print the result*/
+			wrd = letters = odd = even = 0;
+		} else {								 /* whitespace or tab */
+			wrd = letters = 0;
+		}
+	}
+	return 0;
+}
