28da20ae94f37a7d3673b68279d5cf860ea09e4d
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 20:58:13 +0300

    Update 4.09.1b.c (retab)

--- 4 "The C Programming Language"/4.09.1b.c
+++ 4 "The C Programming Language"/4.09.1b.c
@@ -3,23 +3,23 @@
 
 int main()
 {
-	int c, wrd, letters, odd, even;
-	wrd = letters = odd = even = 0;
-	while((c = getchar()) != EOF) {
-		if(c != '\n' && c != ' ' && c != '\t') { /* non-whitespace char */
-			if(!wrd)
-				wrd = 1;						 /* word indicator */
-			letters++;
-			continue;							 /* next char */
-		}
-		if(wrd)									 /* the word is over */	
-			letters % 2 == 0 ? even++ : odd++;	 /* counting letters */
-		if(c == '\n') {							 /* end of the line */
-			printf("odd: %d, even: %d\n", odd, even); /*print the result*/
-			wrd = letters = odd = even = 0;
-		} else {								 /* whitespace or tab */
-			wrd = letters = 0;
-		}
-	}
-	return 0;
+    int c, wrd, letters, odd, even;
+    wrd = letters = odd = even = 0;
+    while((c = getchar()) != EOF) {
+        if(c != '\n' && c != ' ' && c != '\t') { /*non-whitespace char*/
+            if(!wrd)
+                wrd = 1;                         /* word indicator */
+            letters++;
+            continue;                            /* next char */
+        }
+        if(wrd)                                  /* the word is over  */    
+            letters % 2 == 0 ? even++ : odd++;   /* counting letters  */
+        if(c == '\n') {              /* end of the line, print result */
+            printf("odd: %d, even: %d\n", odd, even); 
+            wrd = letters = odd = even = 0;
+        } else {                                 /* whitespace or tab */
+            wrd = letters = 0;
+        }
+    }
+    return 0;
 }
