89aa28be05565e4edb03ed48e98f9d1e99eb7e5f
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 20:55:31 +0300

    Update 4.09.1a.c (retab)

--- 4 "The C Programming Language"/4.09.1a.c
+++ 4 "The C Programming Language"/4.09.1a.c
@@ -3,22 +3,22 @@
 
 int main()
 {
-	int c, n, wrd;					    /* n - number of words */
-	n = wrd = 0;						/* wrd - word's indicator */
-	while((c = getchar()) != EOF) {
-		if(c == '\n') {
-			printf("%d\n", n);
-			n = wrd = 0;
-			continue;
-		} 
-		if(c == ' ' || c == '\t') {
-			wrd = 0;
-			continue;
-		}
-		if(!wrd) {
-			wrd = 1;					  /* new word begins */
-			n++;
-		}
-	}
-	return 0;
+    int c, n, wrd;                      /* n - number of the words */
+    n = wrd = 0;                        /* wrd - word's indicator */
+    while((c = getchar()) != EOF) {
+        if(c == '\n') {
+            printf("%d\n", n);
+            n = wrd = 0;
+            continue;
+        } 
+        if(c == ' ' || c == '\t') {
+            wrd = 0;
+            continue;
+        }
+        if(!wrd) {
+            wrd = 1;                    /* new word begins */
+            n++;
+        }
+    }
+    return 0;
 }
