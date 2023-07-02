ec0a2aaa192ce7ff5dee30142a7b710712a9c762
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 18:23:33 +0300

    4.06.c +

---
+++ 4 "The C Programming Language"/4.06.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int put_sum(int *a, int *b, int *c)
+{
+	int sum = *a + *b + *c;
+	*a = *b = *c = sum;
+	return sum;
+}
+
+int main()
+{
+	int a, b, c, s;
+	scanf("%d%d%d", &a, &b, &c);
+	printf("%d %d %d\n", a, b, c);
+	s = put_sum(&a, &b, &c);
+	printf("%d %d %d %d\n", a, b, c, s);
+	return 0;
+}
