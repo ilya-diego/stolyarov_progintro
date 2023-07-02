182858a35e5633c8fe807db31022e8e765d882e0
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 20:51:10 +0300

    Update 4.06.c (retab)

--- 4 "The C Programming Language"/4.06.c
+++ 4 "The C Programming Language"/4.06.c
@@ -2,17 +2,17 @@
 
 int put_sum(int *a, int *b, int *c)
 {
-	int sum = *a + *b + *c;
-	*a = *b = *c = sum;
-	return sum;
+    int sum = *a + *b + *c;
+    *a = *b = *c = sum;
+    return sum;
 }
 
 int main()
 {
-	int a, b, c, s;
-	scanf("%d%d%d", &a, &b, &c);
-	printf("%d %d %d\n", a, b, c);
-	s = put_sum(&a, &b, &c);
-	printf("%d %d %d %d\n", a, b, c, s);
-	return 0;
+    int a, b, c, s;
+    scanf("%d%d%d", &a, &b, &c);
+    printf("%d %d %d\n", a, b, c);
+    s = put_sum(&a, &b, &c);
+    printf("%d %d %d %d\n", a, b, c, s);
+    return 0;
 }
