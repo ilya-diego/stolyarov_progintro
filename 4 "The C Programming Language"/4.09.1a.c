318184a428d7d84325460cf25a4304a486ef76ba
ilya-diego <134055713+ilya-diego@users.noreply.github.com>
2023-07-02 19:08:31 +0300

    4.09.1a +

---
+++ 4 "The C Programming Language"/4.09.1a.c
@@ -0,0 +1,24 @@
+/* Number of words per line */
+#include <stdio.h>
+
+int main()
+{
+	int c, n, wrd;					    /* n - number of words */
+	n = wrd = 0;						/* wrd - word's indicator */
+	while((c = getchar()) != EOF) {
+		if(c == '\n') {
+			printf("%d\n", n);
+			n = wrd = 0;
+			continue;
+		} 
+		if(c == ' ' || c == '\t') {
+			wrd = 0;
+			continue;
+		}
+		if(!wrd) {
+			wrd = 1;					  /* new word begins */
+			n++;
+		}
+	}
+	return 0;
+}
